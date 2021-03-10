489fbaf7c136e7b6feaab2750096ea0f68bb9ff1
Valentin <valentin@unimplemented.org>
2021-03-10 11:02:14 +0100

    Continue showing launch screen until requestSettings has been called (#110)
    * Resign active handler
    
    * Continue displaying launch screen

--- meteocool/ViewController.swift
+++ meteocool/ViewController.swift
@@ -99,7 +99,7 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, Lo
     override func loadView() {
         super.loadView()
         viewController = self
-        
+
         webView?.configuration.userContentController.add(self, name: "scriptHandler")
         webView?.configuration.userContentController.add(self, name: "timeHandler")
         webView.scrollView.contentInsetAdjustmentBehavior = .never
@@ -192,6 +192,7 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, Lo
         }
         
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.willResignActive), name: UIApplication.willResignActiveNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.injectSettings),
                                                name: NSNotification.Name("SettingsChanged"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
@@ -201,6 +202,7 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, Lo
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+
 
         let locationAction = {
             completion in
@@ -266,9 +268,32 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, Lo
                 })?.presentFrom(self, animated: true)
             }
         }
+
+        if (!webviewReady) {
+            performSegue(withIdentifier: "ShowLaunchScreen", sender: nil)
+        }
+    }
+
+    func hideSplash() {
+        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+
+        if var topController = keyWindow?.rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+
+            topController.dismiss(animated: true)
+        }
+
     }
 
     var alertWindow: UIWindow?
+
+    @objc func willResignActive() {
+        if webviewReady {
+            self.webView.evaluateJavaScript("window.leaveForeground();")
+        }
+    }
 
     @objc func willEnterForeground() {
         if ((userDefaults?.bool(forKey: "autoZoom")) ?? false && webviewReady) {
@@ -497,17 +522,18 @@ window.downloadForecast(function() {
         if action == "requestSettings" {
             webviewReady = true
             injectSettings()
-            
+
             if (locationStateMachine?.state == .off) {
                 if (userDefaults?.bool(forKey: "autoZoom") ?? false && userDefaults?.bool(forKey: "onboardingDone") ?? false){
                     locationStateMachine?.trigger(.buttonPress)
                 }
             }
-            
+
             trippleButton.isHidden = false
             settingsButton.isHidden = false
             layerSwitcherButton.isHidden = false
             positionButton.isHidden = false
+            hideSplash()
         }
         
         if action == "layerSwitcherClosed" {
